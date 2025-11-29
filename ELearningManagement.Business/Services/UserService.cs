9dbf2dc0c5276e54712fe4784f7ffe977ff87e08
nguyenanhktqs-lgtm <nguyenanhktqs@gmail.com>
2025-11-29 13:15:29 +0700

    Add UserService class

---
+++ ELearningManagement.Business/Services/UserService.cs
@@ -0,0 +1,82 @@
+using ELearningManagement.Data.Repositories;
+using ELearningManagement.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELearningManagement.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _userRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        public async Task<User> AuthenticateAsync(string username, string password)
+        {
+            var users = await _userRepository.FindAsync(u => u.Username == username);
+            var user = users.FirstOrDefault();
+
+            if (user == null)
+                return null;
+
+            if (VerifyPassword(password, user.PasswordHash))
+                return user;
+
+            return null;
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            user.PasswordHash = HashPassword(user.PasswordHash);
+            user.CreatedDate = DateTime.Now;
+            user.IsActive = true;
+            await _userRepository.AddAsync(user);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user != null)
+            {
+                await _userRepository.DeleteAsync(user);
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hashedBytes);
+            }
+        }
+
+        private bool VerifyPassword(string password, string hash)
+        {
+            var hashOfInput = HashPassword(password);
+            return hashOfInput == hash;
+        }
+    }
+}
